d5af9a2ccc81f0484226f85e22a7dc4e12281adf
John Doe <johndoe@example.com>
2020-12-11 22:39:21 -0700

    hi

---
+++ server.js
@@ -0,0 +1,9 @@
+const { static } = require('express');
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, "./dist")));
+
+
+app.listen(3000, () => console.log(`Server running on 3000`))
